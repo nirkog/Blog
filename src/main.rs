257ebb16116c9e9abed1a8a54936df9023a7e777
Nir Kogman <nirkog@gmail.com>
2022-01-21 22:22:27 +0200

    Added stuff to about page

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,12 @@ async fn styles(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/style/").join(file)).await.ok()
 }
 
+#[get("/image/<file>")]
+async fn images(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/images/").join(file)).await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, about, styles])
+    rocket::build().mount("/", routes![index, about, styles, images])
 }
