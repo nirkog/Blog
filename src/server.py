f824b990136fb2e00c19e6a85f4cf644c5a68db3
Nir Kogman <nirkog@gmail.com>
2022-11-25 00:28:46 +0200

    Fuck rust

---
+++ src/server.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder=os.path.abspath("./templates/"), static_folder=os.path.abspath("./static/"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/post/<name>")
+def test_post(name):
+    return render_template("post.html", post=render_template(f"posts/{name}.html"))
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html")
